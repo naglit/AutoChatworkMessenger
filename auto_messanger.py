63571688cd38c86bcb7a157568e86780eba39a89
naglit <docono.ne.ip@gmail.com>
2022-05-27 11:09:39 +0900

    changedsome

--- auto_messanger.py
+++ auto_messanger.py
@@ -13,30 +13,29 @@ config.read(backslash + "message.cfg")
 chatwork_config = config["chatwork"]
 
 def main():
-    id = retrieve_arg()
-    print(id)
-    message = read_message(id)
+    message = read_message(retrieve_arg())
     send_message(message)
 
 def retrieve_arg(): return str(sys.argv[1]) if len(sys.argv) > 1 else ""
 
-# read a text in message.txt
 def read_message(id):
-
+    ''' read a text in messages.json '''
     if id == "":
-        print("An id should be taken as an argument ")
+        print("This script needs to take an argument.")
         return
 
     # read
     with open(backslash + 'messages.json', "r", encoding="utf-8") as f:
-        # lines = f.readlines()
         messages = json.load(f)["messages"]
         for message in messages:
             if message["id"] == id: return message
 
-# send a message on Chatowork
+
 def send_message(message):
-    print(message)
+    ''' send a message on Chatowork'''
+    if message == None:
+        return
+
     url = chatwork_config["chatwork_url"] + '/rooms/' + message["room_id"] + '/messages?force=1'
     headers = { 'X-ChatWorkToken': chatwork_config["api_key"] }
     params = { 'body': message["body"] }
